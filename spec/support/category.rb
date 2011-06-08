23cd921be0a9b1397cc5e7380ac723f0e37f86cd
Jan Roesner <jan@roesner.it>
2011-06-08 12:55:08 +0200

    doc tests

--- spec/support/category.rb
+++ spec/support/category.rb
@@ -4,8 +4,6 @@ class Category
   include Mongoid::Document
   include Mongoid::Timestamps
 
-  # has_mongestry
-
   field :name, type: String
   field :persisted_depth, type: Integer
   field :ancestry, type: String
